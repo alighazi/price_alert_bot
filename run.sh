d1001f623280531912c6f78379066f2ead939edf
Raymond Lowe <raymondclowe@gmail.com>
2021-10-04 08:57:14 +0800

    make it work as service

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 cd /home/rcl/price_alert_bot
 pwd
 source venv/bin/activate
-logfile="nohup.out"
+logfile="price.log"
 if [ -f "$logfile" ] 
 then mv "$logfile" "$logfile.$(date +%F-%T)"
 fi
-nohup python3 tg_bot_service.py &
-
-
+python3 tg_bot_service.py 
