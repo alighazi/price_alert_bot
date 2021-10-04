78ff5d37333d1ffd783fa3985374619fb5f39db5
Raymond Lowe <raymondclowe@gmail.com>
2021-10-04 08:48:39 +0800

    revert to (similar to) original run.sh)

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 cd /home/rcl/price_alert_bot
 pwd
 source venv/bin/activate
+logfile="nohup.out"
+if [ -f "$logfile" ] 
+then mv "$logfile" "$logfile.$(date +%F-%T)"
+fi
+nohup python3 tg_bot_service.py &
 
-python3 tg_bot_service.py
 
