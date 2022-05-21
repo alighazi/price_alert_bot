c7d0e0b43dd66ac07884eb9b182dec9149a12c80
alighazi <ali.msgh@gmail.com>
2022-05-21 13:19:15 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
-cd /home/rcl/price_alert_bot
-pwd
 source venv/bin/activate
 logfile="price.log"
 if [ -f "$logfile" ] 
 then mv "$logfile" "$logfile.$(date +%F-%T)"
 fi
-python3 tg_bot_service.py debug
+nohup python3 tg_bot_service.py &
